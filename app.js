3e8f1eb2cfce72a3ac7896bf94cf716a994b9e1d
Neerhof <nneerhof@una.edu>
2025-01-21 17:04:33 -0600

    bye

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname +'/public'))
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html');
+
+})
+
+app.get('/ejs', function (req, res) {
+  res.render('word',
+    {pageTitle: 'my cool ejs page'}
+  );
+})
+
+app.get('/nodemon', function (req, res) {
+  res.send('...');
+
+})
 
 //endpoint, middleware(s)
-app.get('/', function (req, res) {
-  res.send('Hello Express')
+app.get('/helloRender', function (req, res) {
+  res.send('Hello Express from Real World<br><a href="/">back to home</a>')
 })
 
 app.listen(port, ()=> console.log(`server is running on ... ${port}`));
