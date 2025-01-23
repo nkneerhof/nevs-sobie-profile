3c57bb965bfde1bb94abf4cb3f2ae705321215e5
Neerhof <nneerhof@una.edu>
2025-01-23 16:47:49 -0600

    !

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;  
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname +'/public'))
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(express.static(__dirname + '/public'))
+
+// change my code 
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
 
 })
+
+app.post('/saveMyName', (req,res)=>{
+  console.log('did we hit the post endpoint?'); 
+
+  console.log(req.body); 
+
+
+  // res.redirect('/ejs'); 
+
+  res.render('word',
+  {pageTitle: req.body.myName});
+
+
+  // res.render('words',
+  // {theData : req.body});
+
+
+})
+
+app.get('/saveMyNameGet', (req,res)=>{
+  console.log('did we hit the get endpoint?'); 
+
+  console.log(req.query); 
+
+  res.redirect('/ejs'); 
+
+})
+
 
 app.get('/ejs', function (req, res) {
   res.render('word',
@@ -16,8 +48,9 @@ app.get('/ejs', function (req, res) {
   );
 })
 
+
 app.get('/nodemon', function (req, res) {
-  res.send('...');
+  res.send('look ma, no kill node process then restart node then refresh browser...cool?');
 
 })
 
@@ -26,4 +59,12 @@ app.get('/helloRender', function (req, res) {
   res.send('Hello Express from Real World<br><a href="/">back to home</a>')
 })
 
-app.listen(port, ()=> console.log(`server is running on ... ${port}`));
+
+
+
+app.listen(
+  port, 
+  ()=> console.log(
+    `server is running on ... localhost:${port}`
+    )
+  );
